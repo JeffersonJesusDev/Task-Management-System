1df6873dfa0ab856545f3d7e7ccc5b4db23377c3
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-21 20:57:58 -0300

    Criando novos metodos GetOneId

--- src/main/java/com/jdev/TaskManagementSystem/service/UserService.java
+++ src/main/java/com/jdev/TaskManagementSystem/service/UserService.java
@@ -1,5 +1,6 @@
 package com.jdev.TaskManagementSystem.service;
 
+import com.jdev.TaskManagementSystem.dto.UserDTO;
 import com.jdev.TaskManagementSystem.model.User;
 import com.jdev.TaskManagementSystem.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,8 +17,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+
+    public User createUser(UserDTO data) {
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
     }
 
     public ResponseEntity<List<User>> getAllUsers() {
@@ -24,8 +33,10 @@ public class UserService {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-    public User findUserById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+    public Optional<User> findUserById(Long id) {
+
+
+        return userRepository.findById(id);
     }
+
 }
