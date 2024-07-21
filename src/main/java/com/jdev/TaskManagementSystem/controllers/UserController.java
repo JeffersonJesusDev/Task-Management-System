1e7a99aeba2426fe1082213eefa1778eee78d45f
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-21 20:59:02 -0300

    Criando novas requisições

--- src/main/java/com/jdev/TaskManagementSystem/controllers/UserController.java
+++ src/main/java/com/jdev/TaskManagementSystem/controllers/UserController.java
@@ -1,14 +1,15 @@
 package com.jdev.TaskManagementSystem.controllers;
 
+import com.jdev.TaskManagementSystem.dto.UserDTO;
 import com.jdev.TaskManagementSystem.model.User;
 import com.jdev.TaskManagementSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -20,5 +21,20 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         return this.userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUserById(@PathVariable(value = "id") Long id){
+        Optional<User> userOptional = userService.findUserById(id);
+        if(!userOptional.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found");
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(userOptional.get());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@RequestBody UserDTO user){
+        User newUser = userService.createUser(user);
+        return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 }
