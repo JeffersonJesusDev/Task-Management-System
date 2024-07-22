5b07ab63c4f0b26e4c7b023c669b1976c25deeaf
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-22 18:46:33 -0300

    Adicionando métodos no repository

--- src/main/java/com/jdev/TaskManagementSystem/repositories/TaskRepository.java
+++ src/main/java/com/jdev/TaskManagementSystem/repositories/TaskRepository.java
@@ -4,6 +4,9 @@ import com.jdev.TaskManagementSystem.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long id);
 }
