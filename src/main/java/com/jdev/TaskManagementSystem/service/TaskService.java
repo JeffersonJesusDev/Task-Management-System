14613440541546100531ba7770c9b92c7d8a4ff7
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-22 14:48:19 -0300

    Adicionando método para criação de novas tasks ao meu usuário

--- src/main/java/com/jdev/TaskManagementSystem/service/TaskService.java
+++ src/main/java/com/jdev/TaskManagementSystem/service/TaskService.java
@@ -1,14 +1,21 @@
 package com.jdev.TaskManagementSystem.service;
 
+import com.jdev.TaskManagementSystem.dto.TaskDTO;
+import com.jdev.TaskManagementSystem.dto.UserDTO;
+import com.jdev.TaskManagementSystem.enums.Status;
 import com.jdev.TaskManagementSystem.model.Task;
 import com.jdev.TaskManagementSystem.model.User;
 import com.jdev.TaskManagementSystem.repositories.TaskRepository;
+import com.jdev.TaskManagementSystem.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskService {
@@ -16,16 +23,48 @@ public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     public List<Task> getTasks() {
         return taskRepository.findAll();
     }
 
-    public Task addTask(Task task) {
-        return taskRepository.save(task);
+    public void saveTask(Task task) {
+        this.taskRepository.save(task);
     }
+
+    public Task addTask(TaskDTO task) {
+        Task newTask = new Task();
+        this.saveTask(newTask);
+        return newTask;
+    }
+
+    public ResponseEntity<List<Task>> getAllUsers() {
+        List<Task> tasks = this.taskRepository.findAll();
+        return new ResponseEntity<>(tasks, HttpStatus.OK);
+    }
+
 
     public Task findById(Long id) {
         return taskRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public Task createTaskForUser(Long userId, String taskName, Status status) throws Exception {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            Task task = new Task();
+            task.setTaskName(taskName);
+            task.setStatus(status);
+            task.setUser(user);
+            user.getTask().add(task);
+            taskRepository.save(task);
+            userRepository.save(user);
+            return task;
+        } else {
+            throw new Exception("User not found");
+        }
     }
 }
