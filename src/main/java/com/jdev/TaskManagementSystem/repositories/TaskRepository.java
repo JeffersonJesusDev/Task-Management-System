cdb13d3b74d2b287a38cd1c26491bc3ea292f75a
JeffersonJesusDev <jefferson3680@hotmail.com>
2024-07-23 00:31:26 -0300

    exclusão de metodo desnecessário

--- src/main/java/com/jdev/TaskManagementSystem/repositories/TaskRepository.java
+++ src/main/java/com/jdev/TaskManagementSystem/repositories/TaskRepository.java
@@ -2,7 +2,10 @@ package com.jdev.TaskManagementSystem.repositories;
 
 import com.jdev.TaskManagementSystem.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
